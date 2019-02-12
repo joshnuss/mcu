ae36904b1548bbfde17b5ac04fb6d1e7046ef6dd
Josh Nussbaum <joshnuss@gmail.com>
2019-02-12 11:51:42 -0500

    Adds NAND and NOR

--- lib/mcu/alu.ex
+++ lib/mcu/alu.ex
@@ -5,8 +5,10 @@ defmodule MCU.ALU do
   Arithmetic logic unit
   """
 
-  @op_and 0b0001
-  @op_or  0b0010
+  @op_and  0b0001
+  @op_or   0b0010
+  @op_nand 0b0011
+  @op_nor  0b0100
 
   @type op_code :: <<_::4>>
 
@@ -21,6 +23,10 @@ defmodule MCU.ALU do
       4
       iex> ALU.compute(0b0010, 3, 7) # or
       7
+      iex> ALU.compute(0b0011, 3, 7) # nand
+      -4
+      iex> ALU.compute(0b0100, 3, 7) # nand
+      -8
   """
   def compute(op_code, a, b \\ 0) do
     do_compute(op_code, a, b)
@@ -32,5 +38,13 @@ defmodule MCU.ALU do
 
   defp do_compute(@op_or, a, b) do
     bor(a, b)
+  end
+
+  defp do_compute(@op_nand, a, b) do
+    band(a, b) |> bnot()
+  end
+
+  defp do_compute(@op_nor, a, b) do
+    bor(a, b) |> bnot()
   end
 end
